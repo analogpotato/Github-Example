3a306271c2b2d3e936eaa42dfd6cffc2ed6dd29a
Frank Foster <frank.e.foster@icloud.com>
2020-07-23 18:44:40 -0700

    Initial Commit

---
+++ Github Example/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Github Example
+//
+//  Created by Frank Foster on 7/23/20.
+//  Copyright © 2020 Frank Foster. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
